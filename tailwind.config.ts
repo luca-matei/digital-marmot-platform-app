14146adef1bd60b95cb8600073335e2f83a1b0a7
Luca Matei <dev@lucamatei.eu>
2024-02-29 00:06:46 +0200

    Basic layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#007bff',
+      },
+      backgroundColor: {
+        primary: '#007bff',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
